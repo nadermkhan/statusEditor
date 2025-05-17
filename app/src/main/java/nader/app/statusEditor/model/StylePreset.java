eea0fca26261dc4ed28cd99e4dca01bffedc06b7
nadermkhan <muhammadnadermahbubkhan@gmail.com>
2025-05-18 02:10:51 +0600

    fixing typos

--- app/src/main/java/nader/app/statusEditor/model/StylePreset.java
+++ app/src/main/java/nader/app/statusEditor/model/StylePreset.java
@@ -1,17 +1,22 @@
 package nader.app.statusEditor.model;
 
-
 public class StylePreset {
-    private final String fontPath;
+    private final String name;
+    private final int typefaceStyle;
     private final int color;
 
-    public StylePreset(String fontPath, int color) {
-        this.fontPath = fontPath;
+    public StylePreset(String name, int typefaceStyle, int color) {
+        this.name = name;
+        this.typefaceStyle = typefaceStyle;
         this.color = color;
     }
 
-    public String getFontPath() {
-        return fontPath;
+    public String getName() {
+        return name;
+    }
+
+    public int getTypefaceStyle() {
+        return typefaceStyle;
     }
 
     public int getColor() {
--- app/src/main/java/nader/app/statusEditor/viewmodel/SharedViewModel.java
+++ app/src/main/java/nader/app/statusEditor/viewmodel/SharedViewModel.java
@@ -3,39 +3,77 @@ package nader.app.statusEditor.viewmodel;
 import android.app.Application;
 import android.graphics.Color;
 import android.net.Uri;
+import android.graphics.Typeface;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import nader.app.statusEditor.model.StylePreset;
+
 public class SharedViewModel extends ViewModel {
-	private static SharedViewModel instance;
-	
-	private final MutableLiveData<Uri> imageUri = new MutableLiveData<>();
-	private final MutableLiveData<String> text = new MutableLiveData<>("Your Text Here");
-	private final MutableLiveData<Integer> textColor = new MutableLiveData<>(Color.BLACK);
-	private final MutableLiveData<String> fontPath = new MutableLiveData<>(null);
-	
-	private SharedViewModel() {}
-	
-	public static SharedViewModel getInstance() {
-		if (instance == null) {
-			instance = new ViewModelProvider.AndroidViewModelFactory(
-			new Application() {}).create(SharedViewModel.class);
-		}
-		return instance;
-	}
-	
-	public LiveData<Uri> getImageUri() { return imageUri; }
-	public void setImageUri(Uri uri) { imageUri.setValue(uri); }
-	
-	public LiveData<String> getText() { return text; }
-	public void setText(String value) { text.setValue(value); }
-	
-	public LiveData<Integer> getTextColor() { return textColor; }
-	public void setTextColor(int color) { textColor.setValue(color); }
-	
-	public LiveData<String> getFontPath() { return fontPath; }
-	public void setFont(String font) { fontPath.setValue(font); }
+    private static SharedViewModel instance;
+
+    private final MutableLiveData<Uri> imageUri = new MutableLiveData<>();
+    private final MutableLiveData<String> text = new MutableLiveData<>("Your Text Here");
+    private final MutableLiveData<Integer> textColor = new MutableLiveData<>(Color.BLACK);
+    private final MutableLiveData<String> fontPath = new MutableLiveData<>(null);
+
+    private final List<StylePreset> stylePresets = new ArrayList<>();
+
+    private SharedViewModel() {
+        // Populate default style presets
+        stylePresets.add(new StylePreset("Classic", Typeface.NORMAL, Color.BLACK));
+        stylePresets.add(new StylePreset("Bold", Typeface.BOLD, Color.BLUE));
+        stylePresets.add(new StylePreset("Italic Red", Typeface.ITALIC, Color.RED));
+        stylePresets.add(new StylePreset("Bold Italic Green", Typeface.BOLD_ITALIC, Color.GREEN));
+    }
+
+    public static SharedViewModel getInstance() {
+        if (instance == null) {
+            instance = new ViewModelProvider.AndroidViewModelFactory(new Application())
+                .create(SharedViewModel.class);
+        }
+        return instance;
+    }
+
+    public LiveData<Uri> getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(Uri uri) {
+        imageUri.setValue(uri);
+    }
+
+    public LiveData<String> getText() {
+        return text;
+    }
+
+    public void setText(String value) {
+        text.setValue(value);
+    }
+
+    public LiveData<Integer> getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(int color) {
+        textColor.setValue(color);
+    }
+
+    public LiveData<String> getFontPath() {
+        return fontPath;
+    }
+
+    public void setFont(String font) {
+        fontPath.setValue(font);
+    }
+
+    public List<StylePreset> getStylePresets() {
+        return stylePresets;
+    }
 }
